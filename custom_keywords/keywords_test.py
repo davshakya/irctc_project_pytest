719ed6782f7f9579b5d7b8cfe1cef81a205554ce
Devendra Singh Shakya <davshakya@gmail.com>
2023-12-26 16:36:18 +0530

    selenium driver fixed

--- custom_keywords/keywords_test.py
+++ custom_keywords/keywords_test.py
@@ -7,13 +7,13 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.select import Select
 import pytest
-from twocaptcha import TwoCaptcha
-from PIL import Image
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-from pytesseract import pytesseract
-import time
+# from twocaptcha import TwoCaptcha
+# from PIL import Image
+# from PIL import Image
+# from PIL import ImageDraw
+# from PIL import ImageFont
+# from pytesseract import pytesseract
+# import time
 
 
 def get_from_city():
@@ -26,14 +26,14 @@ def get_to_city():
     return  train_file.iloc[1,1] 
 
 
-def get_captcha_text(driver):
-    with open('Logo.jpg', 'wb') as file:
-        l = driver.find_element(By.XPATH,"//img[@class='captcha-img']")
-        file.write(l.screenshot_as_png)
-    path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-    image_path = r"D:/Dev_Progs/irctc_project/Logo.jpg"
-    img = Image.open(image_path)
-    pytesseract.tesseract_cmd = path_to_tesseract
-    text = pytesseract.image_to_string(img)
-    captcha_text=text[:-1]
-    return  captcha_text
+# def get_captcha_text(driver):
+#     with open('Logo.jpg', 'wb') as file:
+#         l = driver.find_element(By.XPATH,"//img[@class='captcha-img']")
+#         file.write(l.screenshot_as_png)
+#     path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+#     image_path = r"D:/Dev_Progs/irctc_project/Logo.jpg"
+#     img = Image.open(image_path)
+#     pytesseract.tesseract_cmd = path_to_tesseract
+#     text = pytesseract.image_to_string(img)
+#     captcha_text=text[:-1]
+#     return  captcha_text
--- resources_data/xpath_data.py
+++ resources_data/xpath_data.py
@@ -7,9 +7,9 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.select import Select
 import pytest
-from pytesseract import pytesseract
-import pandas as pd
-import custom_keywords.keywords_test as keywords_test
-
-class Location(driver):
-        city_name_loc  
+# from pytesseract import pytesseract
+# import pandas as pd
+# import custom_keywords.keywords_test as keywords_test
+#
+# class Location(driver):
+#         city_name_loc
