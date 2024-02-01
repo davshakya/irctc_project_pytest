a56e5f33104ba2944ce3cc65eee71ca7b6398384
Devendra Singh Shakya <davshakya@gmail.com>
2024-02-01 09:57:09 +0530

    selenium issue fixed

--- custom_keywords/keywords_test.py
+++ custom_keywords/keywords_test.py
@@ -1,11 +1,11 @@
 import  pandas as pd
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.select import Select
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.common.by import By
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.support.select import Select
 import pytest
 # from twocaptcha import TwoCaptcha
 # from PIL import Image
