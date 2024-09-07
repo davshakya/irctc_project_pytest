ad8eb56cb0035bf4c284949efd0d690912d6dd7d
Devendra Singh Shakya <davshakya@gmail.com>
2024-09-07 08:55:36 +0530

    Revert "demo_branch commit done"
    This reverts commit be75b4c791f5f0370c8d0d53b7a455e009cd0c06.

--- Test_Cases/home_page_test.py
+++ Test_Cases/home_page_test.py
@@ -27,6 +27,3 @@ def test_homepage():
     # driver.find_element(By.XPATH,"//span[contains(text(),'-- Stations --')]/parent::li/following-sibling::li").click()
 
 test_homepage()
-
-
-#hello this is demo_branch commit
